8e99f6471288bc7653ea3cd4a005ee5882f92a1e
Maria Leitão <malufln.99@gmail.com>
2024-08-02 17:43:46 -0300

    fix: add primary column to ProductCharacteristic entity

--- src/product/product-characteristics.entity.ts
+++ src/product/product-characteristics.entity.ts
@@ -1,7 +1,10 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('product_characteristics')
 export class ProductCharacteristic {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
   @Column({ name: 'name', length: 100, nullable: false })
   name: string;
 
