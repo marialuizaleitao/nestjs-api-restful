2f8f98a4dbea03cd9c099298e9bd3bc6c901d73d
Maria Leitão <malufln.99@gmail.com>
2024-08-02 17:16:51 -0300

    feat: update TypeOrmConfigService to include entity path for TypeORM

--- src/config/typeorm-config.service.ts
+++ src/config/typeorm-config.service.ts
@@ -13,7 +13,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       username: this.configService.get<string>('DB_USERNAME'),
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_NAME'),
-      entities: [],
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
     };
   }
