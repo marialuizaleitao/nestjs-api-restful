546f85a4a48f9aaa8f012bca47be737d29bd2018
Maria Leitão <malufln.99@gmail.com>
2024-08-02 14:31:56 -0300

    refactor: remove unused annotation

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
-import { IsEmail } from 'class-validator';
 import { IsEmailUnique } from './validation/is-email-unique';
 
 @Module({
