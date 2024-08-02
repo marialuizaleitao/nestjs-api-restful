d245ed6b7a14736eaa7742411030731298ffcf8f
Maria Leitão <malufln.99@gmail.com>
2024-08-02 16:56:26 -0300

    chore: refactor UserEntity to integrate TypeORM

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,21 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
   email: string;
+
+  @Column({ name: 'password', length: 255, nullable: false })
   password: string;
 }
