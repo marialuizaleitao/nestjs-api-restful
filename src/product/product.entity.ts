74e1d564a6978cd9d430edff818466f63872ef71
Maria Leitão <malufln.99@gmail.com>
2024-08-02 17:16:12 -0300

    feat: update ProductEntity with additional fields

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { ProductCharacteristic } from './product-characteristics.entity';
 import { ProductImage } from './product-image.entity';
 
@@ -24,6 +24,15 @@ export class ProductEntity {
 
   @Column({ name: 'category', length: 100, nullable: false })
   category: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 
   characteristics: ProductCharacteristic[];
   images: ProductImage[];
