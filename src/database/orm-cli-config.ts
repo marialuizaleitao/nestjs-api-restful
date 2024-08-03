54488055072b090272efac92439f9bc7c0333bef
Maria Leitão <malufln.99@gmail.com>
2024-08-03 19:33:56 -0300

    feat: register migrations

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,11 +1,17 @@
+import { CreateUsersTable1722710609574 } from 'src/migrations/1722710609574-CreateUsersTable';
+import { CreateProductsTable1722721968156 } from 'src/migrations/1722721968156-CreateProductsTable';
+import { CreateProductCharacteristicsTable1722722136350 } from 'src/migrations/1722722136350-CreateProductCharacteristicsTable';
+import { CreateProductImagesTable1722722669760 } from 'src/migrations/1722722669760-CreateProductImagesTable';
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
-import { CreateUsersTable } from 'src/migrations/1722710609574-CreateUsersTable';
-import { CreateProductsTable } from 'src/migrations/1722721968156-CreateProductsTable';
-import { CreateProductCharacteristicsTable } from 'src/migrations/1722722136350-CreateProductCharacteristicsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateUsersTable, CreateProductsTable, CreateProductCharacteristicsTable],
+  migrations: [
+    CreateUsersTable1722710609574,
+    CreateProductsTable1722721968156,
+    CreateProductCharacteristicsTable1722722136350,
+    CreateProductImagesTable1722722669760,
+  ],
 });
