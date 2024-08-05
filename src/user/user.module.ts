472c58636cee7bfdc96f2c1f02b729dd7aac6c3a
Maria Leitão <malufln.99@gmail.com>
2024-08-05 13:17:54 -0300

    chore: remove unused imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
-import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import { IsEmailUnique } from './validation/is-email-unique';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService, UserRepository, IsEmailUnique],
+  providers: [UserService, IsEmailUnique],
 })
 export class UserModule {}
